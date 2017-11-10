b48b893c7982cd228e82ab5baff37f2b656c98f2
LyssaStiller <stiller108@gmail.com>
2017-11-10 17:19:36 -0500

    modelSetup

---
+++ models/index.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack');
+
+const User = db.define('user', {
+    name: {
+        type : Sequelize.STRING
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+});
+
+const Page = db.define('page', {
+    title: { 
+        type: Sequelize.STRING
+    },
+    urlTitle: {
+        type: Sequelize.STRING
+    },
+    content: {
+        type: Sequelize.TEXT
+    },
+    status: {
+        type: Sequelize.ENUM('open', 'closed')
+    }
+});
+
+module.exports = {
+    Page: Page,
+    User: User
+};
