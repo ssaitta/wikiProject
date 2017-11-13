4b9bb08e9d6e100ecc09d2567e89cd28bd763403
Sierra Saitta-Zelterman <ssaitta13@gmail.com>
2017-11-13 11:53:04 -0500

    add a page page looks good

--- routes/wiki.js
+++ routes/wiki.js
@@ -2,15 +2,19 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', function(req, res, next) {
-    res.send('got to GET /wiki/');
+    //res.send('got to GET /wiki/');
+    //res.render('wikipage')
+    res.redirect('/');
   });
   
   routes.post('/', function(req, res, next) {
+    
     res.send('got to POST /wiki/');
   });
   
   routes.get('/add', function(req, res, next) {
-    res.send('got to GET /wiki/add');
+    res.render('addpage.html');
+    //res.send('got to GET /wiki/add');
   });
 
 module.exports = routes
